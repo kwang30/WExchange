61010f3e8b98c9a939d1dc9e03e7c9e8b10bcb89
Kevin <kwang30@gmail.com>
2017-10-11 23:04:40 -0400

    stripe pay and comfirm

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,35 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+  before_action :set_description
+  # before_action :authenticate_user!
+
   def new
   end
 
   def create
-    # Amount in cents
-    @amount = 500
+    customer = StripeTool.create_customer(email: params[:stripeEmail],
+                                          stripe_token: params[:stripeToken])
 
-    user = Stripe::User.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :user    => user.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe user',
-      :currency    => 'usd'
-    )
+    charge = StripeTool.create_charge(customer_id: customer.id,
+                                      amount: @amount,
+                                      description: @description)
+    redirect_to thanks_path
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  def thanks
+  end
+
+
+  private
+    def set_description
+      @description = "Some amazing product"
+    end
+
+    def amount_to_be_charged
+      @amount = 10000
+    end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,27 @@ class RequestsController < ApplicationController
   def requests
   end
 
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def thanks
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
 
 
 end
