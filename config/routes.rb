f2f3d686056fdd5f2741ce2b8f3e19723d60e96c
Ariunbold Batjargal <abubat@brandeis.edu>
2017-10-17 13:31:14 -0400

    Added discover page, login page and sign up page. Also changed navbar look.

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,27 @@ Rails.application.routes.draw do
 
   get 'login/main'
 
+  get '/signup', to: 'signup#main'
+
+  get '/', to: 'home#home'
+
   root 'home#home'
+
   get '/notifications', to: 'notifications#notifications'
+
   get '/profile', to: 'users#profile'
   get '/profile/edit', to: 'settings#edit_user'
   get '/profile/request', to: 'requests#requests'
+
   get '/discover', to: 'discover#main'
 
-
+  get '/login', to: 'login#main'
 
   get '/settings', to: 'settings#index'
   get '/settings/profile', to: 'settings#edit_user'
-  get '/settings/card', to: 'settings#edit_billing'
+  get '/settings/billing', to: 'settings#edit_billing'
   get '/settings/advanced', to: 'settings#edit_advanced'
   get '/settings/notifications', to: 'settings#edit_notifications'
-
 
   resources :charges
   resources :users
