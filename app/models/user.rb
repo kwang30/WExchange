f18abaa7050c0091f12295b8bd3131b73211204a
Kevin <kwang30@gmail.com>
2017-10-20 00:45:23 -0400

    added model authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
     has_many :transactions
     has_many :portfolios
     has_many :messages
+
+    before_save { self.email = email.downcase }
+    before_save { self.username = email.username }
+    has_secure_password
+
+    validates :name,  presence: true, length: {maximum: 50 },
+    uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+\.[edu]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+    validates :password, presence: true, length: { minimum: 6 }
+
 end
