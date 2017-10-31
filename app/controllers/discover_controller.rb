ac262bb22cd92bb46aa216d1afb43a5a2f3aea06
Alec Rodgers <alecr1997@gmail.com>
2017-10-30 21:21:49 -0400

    Display users on discover page

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,14 +1,22 @@
 class DiscoverController < ApplicationController
   def main
-    if params[:q].nil?
-      @users = Array.new
+    if params[:search_query].nil?
+      @users = User.all
     else
-      @users = User.where("first_name like ?",  "%#{params[:q]}%")
+      @users = User.where("first_name like ?",  "%#{params[:search_query]}%")
     end
   end
 
   def index
     render 'main'
+  end
+
+  def show
+    if params[:search_query].nil?
+      @users = Array.new
+    else
+      @users = User.where("first_name like ?",  "%#{params[:search_query]}%")
+    end
   end
 
 end
