e1a3b0b939b744459ebce2a52d7786e609a1feee
paulcabrera93 <paulkcabrera@gmail.com>
2017-12-13 19:17:55 -0500

    Add log_in_as method

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,8 +2,18 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:sally)
+    log_in_as @user
+  end
+
   test "should get new" do
     get login_path
     assert_response :success
+  end
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
   end
 end
