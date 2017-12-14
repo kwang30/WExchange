c4c27ee8682e151583dbdb907016fb3b10664af7
Alec Rodgers <alecr1997@gmail.com>
2017-12-14 12:29:22 -0500

    Fix bug with message notifications

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,14 @@ class MessagesController < ApplicationController
   def create
     @chat = Chat.includes(:recipient).find(params[:chat_id])
     @message = @chat.messages.create(message_params)
-    Notification.create(recipient: User.find_by(id: @chat.recipient_id), actor: User.find_by(id: @chat.sender_id), action: "sent you a message", notifiable: @chat, destination_url: chats_path)
+    if @message.user_id == @chat.sender_id
+      sender_id = @chat.sender_id
+      recipient_id = @chat.recipient_id
+    elsif @message.user_id == @chat.recipient
+      sender_id = @chat.recipient_id
+      recipient_id = @chat.sender_id
+    end
+    Notification.create(recipient: User.find_by(id: recipient_id), actor: User.find_by(id: sender_id), action: "sent you a message", notifiable: @chat, destination_url: chats_path)
     respond_to do |format|
       format.js
     end
