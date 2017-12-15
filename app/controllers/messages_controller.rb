3a62f50bfe473a94c5a3ef6654249efb6b6930cc
Alec Rodgers <alecr1997@gmail.com>
2017-12-15 14:12:35 -0500

    Redesign messaging architecture

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,11 +5,11 @@ class MessagesController < ApplicationController
     if @message.user_id == @chat.sender_id
       sender_id = @chat.sender_id
       recipient_id = @chat.recipient_id
-    elsif @message.user_id == @chat.recipient
+    elsif @message.user_id == @chat.recipient_id
       sender_id = @chat.recipient_id
       recipient_id = @chat.sender_id
     end
-    Notification.create(recipient: User.find_by(id: recipient_id), actor: User.find_by(id: sender_id), action: "sent you a message", notifiable: @chat, destination_url: chats_path)
+    @notif = Notification.create(recipient: User.find_by(id: recipient_id), actor: User.find_by(id: sender_id), action: "sent you a message", notifiable: @chat, destination_url: chats_path)
     respond_to do |format|
       format.js
     end
