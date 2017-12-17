ef332b56f777ad44fccd71223a43909f18750c4b
Kevin <kwang30@gmail.com>
2017-12-16 20:25:35 -0500

    precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,6 @@
 //= require nouislider
 //= require mixitup
 //= require select2
-
 //= require twitter/typeahead
 //= require materialize-sprockets
 //= require dropzone
@@ -60,7 +59,12 @@ $(document).ready( function(){
 
   $('select').material_select();
   $('select').material_select('destroy');
+
 });
+
+
+
+
 
 function displayDashboardTab(tabName) {
   if (tabName == "outgoing") {
@@ -68,7 +72,7 @@ function displayDashboardTab(tabName) {
   } else if (tabName == "incoming") {
     $('ul.tabs').tabs('select_tab', 'dashboard-sidebar-incoming-requests');
   }
-}
+};
 
 
 $(function () {
@@ -76,52 +80,4 @@ $(function () {
     $.get($("#discover-search-form").attr("action"), $("#discover-search-form").serialize(), null, "script");
     return false;
   });
-});
-
-$(function () {
-  $("#tag_search").on('keyup', function () {
-    $.get($("#discover-search-form").attr("action"), $("#discover-search-form").serialize(), null, "script");
-    return false;
-  });
-});
-
-$(".tag_search").select2();
-$(document).ready(function() {
-  $('select#box_assoc_items').select2({
-    placeholder: 'choose items',
-    multiple: true
-  });
-});
-
-
-
-
-$(".select2").select2({
-  tags: true,
-  tokenSeparators: [",", " "],
-  createSearchChoice: function(term, data) {
-    if ($(data).filter(function() {
-      return this.text.localeCompare(term) === 0;
-    }).length === 0) {
-      return {
-        id: term,
-        text: term
-      };
-    }
-  },
-  multiple: true,
-  ajax: {
-    url: '/discover/results.json',
-    dataType: "json",
-    data: function(term, page) {
-      return {
-        q: term
-      };
-    },
-    results: function(data, page) {
-      return {
-        results: data
-      };
-    }
-  }
 });
--- app/assets/javascripts/discover.coffee.js
+++ app/assets/javascripts/discover.coffee.js
@@ -2,10 +2,4 @@
 function searchData() {
   $.get($("#discover-search-form").attr("action"), $("#discover-search-form").serialize(), null, "script");
   return false;
-}
-
-
-function searchData() {
-  $.get($("#discover-search-form").attr("action"), $("#discover-search-form").serialize(), null, "script");
-  return false;
-}
+};
